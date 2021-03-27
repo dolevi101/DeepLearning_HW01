f836c29bed8d6a2192821c78c45b7c84eb2c4591
Dolev <dolevi101@gmail.com>
2021-03-27 15:35:45 +0300

    added the linear_activation_forward function

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from numpy import random
 
 def initialize_parameters(layer_dims):
     params_dict = {"w": [], "b": []}
-    params_dict["w"].append(np.random.rand(1))
+    params_dict["w"].append(np.random.randn(1))
     params_dict["b"].append(np.zeros(1))
     for i in range(1, len(layer_dims)):
-        params_dict["w"].append(np.random.rand(layer_dims[i], layer_dims[i - 1]))
+        params_dict["w"].append(np.random.randn(layer_dims[i], layer_dims[i - 1]))
         params_dict["b"].append(np.zeros(layer_dims[i]))
     return params_dict
 
@@ -30,11 +30,20 @@ def relu(Z):
     activation_cache = {"Z": Z}
     return A, activation_cache
 
+
+def linear_activation_forward(A_prev, W, B, activation):
+    Z, linear_cache = linear_forward(A_prev, W, B)
+    if activation == "softmax":
+        A, activation_cache = softmax(Z)
+    else:
+        A, activation_cache = relu(Z)
+
+    cache = dict(linear_cache)
+    cache.update(activation_cache)
+    return A, cache
+
+
 if __name__ == '__main__':
     result = initialize_parameters([3, 4, 1])
-    #print(result["w"])
-    #print()
-    #print(result["b"])
-    a = [1, 2, 4]
-    r = softmax(a)
-    print(sum(r[0]))
+    print(result)
+
