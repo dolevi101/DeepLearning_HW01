5003897070a46f31190b9b573c9c5154057b33be
Dolev <dolevi101@gmail.com>
2021-03-27 14:57:43 +0300

    added the linear_forward function

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ from numpy import random
 
 
 def initialize_parameters(layer_dims):
-    dict = {"w": [], "b": []}
-    dict["w"].append(np.random.rand(1))
-    dict["b"].append(np.zeros(1))
+    params_dict = {"w": [], "b": []}
+    params_dict["w"].append(np.random.rand(1))
+    params_dict["b"].append(np.zeros(1))
     for i in range(1, len(layer_dims)):
-        dict["w"].append(np.random.rand(layer_dims[i], layer_dims[i-1]))
-        dict["b"].append(np.zeros(layer_dims[i]))
-    return dict
+        params_dict["w"].append(np.random.rand(layer_dims[i], layer_dims[i - 1]))
+        params_dict["b"].append(np.zeros(layer_dims[i]))
+    return params_dict
+
+
+def linear_forward(A, W, b):
+    Z = np.dot(W, A) + b
+    linear_cache = {"A": A, "W": W, "b": b}
+    return Z, linear_cache
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    result = initialize_parameters([3,4,1])
+    result = initialize_parameters([3, 4, 1])
     print(result["w"])
     print()
     print(result["b"])
