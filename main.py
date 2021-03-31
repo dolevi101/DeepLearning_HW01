28a8ba48c00dbc77fe5626aea11d16a8f96168ca
Itai Hay <3392524+itaihay@users.noreply.github.com>
2021-03-31 19:26:03 +0300

    Created stubs for all functions

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numpy import random
 
 
 def initialize_parameters(layer_dims):
@@ -20,7 +19,7 @@ def linear_forward(A, W, b):
 
 def softmax(Z):
     sum_z = sum(np.exp(Z))
-    A = [np.exp(z)/sum_z for z in Z]
+    A = [np.exp(z) / sum_z for z in Z]
     activation_cache = {"Z": Z}
     return A, activation_cache
 
@@ -43,7 +42,84 @@ def linear_activation_forward(A_prev, W, B, activation):
     return A, cache
 
 
+def L_model_forward(X, parameters, use_batchnorm):
+    # TODO
+    cache = None
+    AL = None
+
+    return AL, cache
+
+
+def compute_cost(AL, Y):
+    # TODO
+    cost = None
+    return cost
+
+
+def apply_batchnorm(A):
+    # TODO
+    NA = None
+
+    return NA
+
+
+def linear_backward(dZ, cache):
+    # TODO
+    dA_prev = None
+    dW = None
+    db = None
+
+    return dA_prev, dW, db
+
+
+def linear_activation_backward(dA, cache, activation):
+    # TODO
+    dA_prev = None
+    dW = None
+    db = None
+
+    return dA_prev, dW, db
+
+
+def relu_backward(dA, activation_cache):
+    # TODO
+    dZ = None
+
+    return dZ
+
+
+def softmax_backward(dA, activation_cache):
+    # TODO
+    dZ = None
+
+    return dZ
+
+
+def l_model_backward(AL, Y, caches):
+    # TODO
+    grads = dict()
+
+    return grads
+
+
+def update_parameters(parameters, grads, learning_rate):
+    # TODO
+    return parameters
+
+
+def l_layer_model(X, Y, layers_dims, learning_rate, num_iterations, batch_size):
+    # TODO
+    parameters = None
+    costs = None
+
+    return parameters, costs
+
+def predict(X, Y, parameters):
+    # TODO
+    accuracy = None
+    return accuracy
+
+
 if __name__ == '__main__':
     result = initialize_parameters([3, 4, 1])
     print(result)
-
