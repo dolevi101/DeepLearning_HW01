534d1645124ae7b2a24e7f650a16a53614a6d969
Itai Hay <3392524+itaihay@users.noreply.github.com>
2021-04-03 19:38:14 +0300

    Finished main

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 import keras
 import numpy as np
 from keras.datasets import mnist
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 
@@ -212,7 +211,7 @@ def l_layer_model(X, Y, layers_dims, learning_rate, num_iterations, batch_size, 
     iteration_counter = 0
     best_validation_acc = 0
     validation_acc_not_improved = 0
-    parameter_saver = list()
+    all_parameter_saver = list()
 
     for epoch in range(min_epochs):
         for X_batch, Y_batch in next_batch(X_train, y_train, batch_size):
@@ -222,9 +221,9 @@ def l_layer_model(X, Y, layers_dims, learning_rate, num_iterations, batch_size, 
             parameters = update_parameters(parameters, gradients, learning_rate)
 
             validation_acc = predict(X_val, y_val, parameters)
-            #train_acc = predict(X_train, y_train, parameters)
+            # train_acc = predict(X_train, y_train, parameters)
 
-            parameter_saver.append(
+            all_parameter_saver.append(
                 {"iteration": iteration_counter,
                  "epoch": epoch,
                  "cost": cost,
@@ -240,11 +239,11 @@ def l_layer_model(X, Y, layers_dims, learning_rate, num_iterations, batch_size, 
                 validation_acc_not_improved += 1
 
             if validation_acc_not_improved >= 100:
-                return parameters, parameter_saver
+                return parameters, all_parameter_saver
 
             iteration_counter += 1
 
-    return parameters, parameter_saver
+    return parameters, all_parameter_saver
 
 
 def predict(X, Y, parameters):
@@ -260,62 +259,29 @@ def predict(X, Y, parameters):
     return accuracy
 
 
-def print_shapes(x_train, y_train, x_test, y_test):
-    print(f"x_train.shape = {x_train.shape}")
-    print(f"y_train.shape = {y_train.shape}\n")
-    print(f"x_test.shape = {x_test.shape}")
-    print(f"y_test.shape = {y_test.shape}\n")
+if __name__ == '__main__':
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-
-def prepare_data(x_train, y_train, x_test, y_test):
-    """
-    Perform one-hot encoding to the labels, and reshaping to the data
-    """
     num_classes = len(np.unique(y_train))
     y_train = keras.utils.to_categorical(y_train.reshape(-1, 1), num_classes)
     y_test = keras.utils.to_categorical(y_test.reshape(-1, 1), num_classes)
 
     image_size = 784
-    x_train = x_train.reshape(-1, image_size)
-    x_test = x_test.reshape(-1, image_size)
+    X_train = X_train.reshape(-1, image_size)
+    X_test = X_test.reshape(-1, image_size)
 
-    return x_train, y_train, x_test, y_test
-
-
-def plot(to_plot, title='Title', xlabel='', ylabel=''):
-    import matplotlib.pyplot as plt
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.plot(to_plot)
-    plt.show()
-
-
-if __name__ == '__main__':
-    # result = initialize_parameters([3, 4, 1])
-    # print(result)
-
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
-    print_shapes(X_train, y_train, X_test, y_test)
-    print("classes = ", list(np.unique(y_train)))
-
-    # data preprocessing
-    X_train, y_train, X_test, y_test = prepare_data(X_train, y_train, X_test, y_test)
-    print_shapes(X_train, y_train, X_test, y_test)
-    # X_train, X_test = scale_data(X_train, X_test)
-
-    hidden_dims = [20, 7, 5, 10]
+    hidden_layer_dims = [20, 7, 5, 10]
     lr = 0.009
-    iters = 9000000
+    num_iterations = 9000000
     min_epochs = 50
+    tested_batch_sizes = [32, 64, 128]
 
-    # batch sizes experiment
-    batch_experiment_results = {}
-    for batch_size in [32, 64, 128]:
-        batch_experiment_results[batch_size] = l_layer_model(X_train.T, y_train.T,
-                                                             hidden_dims,
-                                                             learning_rate=lr,
-                                                             batch_size=batch_size,
-                                                             use_batchnorm=False,
-                                                             num_iterations=iters,
-                                                             min_epochs=min_epochs)
+    results = {}
+    for batch_size in tested_batch_sizes:
+        results[batch_size] = l_layer_model(X_train.T, y_train.T,
+                                            hidden_layer_dims,
+                                            learning_rate=lr,
+                                            batch_size=batch_size,
+                                            use_batchnorm=False,
+                                            num_iterations=num_iterations,
+                                            min_epochs=min_epochs)
